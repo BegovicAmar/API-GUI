629aebd6975dfc798849e9f2af6eac2689b01970
BegovicAmar <amar.begovic@mews.com>
2023-10-18 16:52:45 +0200

    added sidebar toggle

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import clsx from 'clsx';
 
 interface SidebarProps {
@@ -6,8 +6,32 @@ interface SidebarProps {
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ mode }) => {
+    useEffect(() => {
+        const sidebarToggleButton = document.querySelector('.sidebar-toggle-button') as HTMLElement;
+        const sidebarMenu = document.querySelector('.sidebar-menu') as HTMLElement;
+
+        const handleToggle = () => {
+            if (sidebarMenu.style.left === '-250px' || sidebarMenu.style.left === '') {
+                sidebarMenu.style.left = '0px';
+            } else {
+                sidebarMenu.style.left = '-250px';
+            }
+        };
+
+        if (sidebarToggleButton && sidebarMenu) {
+            sidebarToggleButton.addEventListener('click', handleToggle);
+        }
+
+        return () => {
+            if (sidebarToggleButton) {
+                sidebarToggleButton.removeEventListener('click', handleToggle);
+            }
+        };
+    }, []);
+
     return (
         <div className="sidebar-container">
+            <button className="sidebar-toggle-button">☰</button>
             <nav
                 className={clsx('sidebar-menu', {
                     'dark-sidebar': mode === 'dark',
